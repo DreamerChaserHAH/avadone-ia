eb50a44ceea8d3b9dd3c147c92a06dd8c727884a
mantelli96 <mantelli96@libero.it>
2024-03-03 14:26:05 +0100

    Added todorepo and added it to the service collection, installed the needed Microsoft.Extentions.DependencyInjection

---
+++ Avadone-ia.Infrastructure/DependencyInjection.cs
@@ -0,0 +1,15 @@
+﻿using Avadone_ia.Application.Common.Interfaces.Persistence;
+using Avadone_ia.Infrastructure.Persistence;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace Avadone_ia.Infrastructure;
+
+public static class DependencyInjection
+{
+    public static IServiceCollection AddInfrastructure(this IServiceCollection services)
+    {
+        services.AddScoped<ITodoRepository, TodoRepository>();
+
+        return services;
+    }
+}
---
+++ Avadone-ia.Infrastructure/Persistence/TodoRepository.cs
@@ -0,0 +1,38 @@
+﻿using Avadone_ia.Application.Common.Interfaces.Persistence;
+using Avadone_ia.Domain;
+using System.Linq.Expressions;
+
+namespace Avadone_ia.Infrastructure.Persistence;
+
+public class TodoRepository : ITodoRepository
+{
+    public void Add(ToDo entity)
+    {
+        throw new NotImplementedException();
+    }
+
+    public void Delete(ToDo entity)
+    {
+        throw new NotImplementedException();
+    }
+
+    public bool Find(Expression<Func<ToDo, bool>> predicate)
+    {
+        throw new NotImplementedException();
+    }
+
+    public IList<ToDo> GetAll()
+    {
+        throw new NotImplementedException();
+    }
+
+    public ToDo GetById(int id)
+    {
+        throw new NotImplementedException();
+    }
+
+    public void Update(ToDo entity)
+    {
+        throw new NotImplementedException();
+    }
+}
