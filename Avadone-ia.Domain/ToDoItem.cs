1a91bf20bdbfb8bb3abae326e92dcebf4973feaf
mantelli96 <mantelli96@libero.it>
2024-03-03 13:53:35 +0100

    basic domain todo implementation

---
+++ Avadone-ia.Domain/ToDoItem.cs
@@ -0,0 +1,16 @@
+﻿namespace Avadone_ia.Domain;
+
+//lets keep model simple with primitive types
+public class ToDoItem
+{
+    private ToDoItem() { } // Ef ctor
+    public string Description { get; private set; } = null!;
+    public bool IsCompleted { get; private set; }
+
+    public ToDoItem(string description) =>
+        Description = description ?? throw new ArgumentNullException(nameof(description));
+
+    public void MarkAsCompleted() => IsCompleted = true;
+   
+}
+
