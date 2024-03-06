436db2ced5102935e38dcdf2fe2efeeaa4446135
Rubanov <bykr3m@gmail.com>
2024-03-06 12:43:36 +0300

    Added AvadoneiaContext class for SQLite tables in Infrastructure.

---
+++ Avadone-ia.Infrastructure/DataBase/AvadoneiaContext.cs
@@ -0,0 +1,22 @@
+﻿using Avadone_ia.Domain;
+using Microsoft.EntityFrameworkCore;
+
+namespace Avadone_ia.Infrastructure.DB;
+
+public class AvadoneiaContext : DbContext
+{
+    #region EF_Tables
+    public DbSet<ToDo> ToDo => Set<ToDo>();
+    public DbSet<ToDoItem> ToDoItem => Set<ToDoItem>();
+    #endregion
+
+    public AvadoneiaContext()
+    {
+        Database.EnsureCreated();
+    }
+
+    protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+    {
+        optionsBuilder.UseSqlite("Data Source=userdata.db");
+    }
+}
